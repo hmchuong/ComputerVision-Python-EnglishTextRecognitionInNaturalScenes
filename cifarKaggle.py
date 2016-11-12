8c64e35ab24f7297237357fee6e0cae8a8ef124d
hmc1108 <minhchuong.itus@gmail.com>
2016-11-12 22:08:48 +0700

    [Update] CirfarKaggle class

--- cifarKaggle.py
+++ cifarKaggle.py
@@ -12,4 +12,84 @@ from matplotlib import pyplot as plt
 class CifarKaggle():
 	"""
 	Lớp này để xử lý các ảnh không có văn bản
-	Một thể hiện của lớp này được tạo ra bên trong lớp 
+	Một thể hiện của lớp này được tạo ra 
+	bên trong phương thức merge_with_cifarkaggle của lớp OCR
+	để trộn dữ liệu CIFAR-10 dataset on Kaggle với dữ liệu có 
+	văn bản của tập 74K Chars dataset
+	"""
+
+	def __init__(self, config):
+		"""
+		Lấy các tham số từ 1 file config.py (kiểu json)
+		"""
+		self.config = self._load_config(config)
+		self.img_size = self.config['img_size']
+		self.folder = self.config['folder']
+		self.verbose = self.config['verbose']
+		self.from_pickle = self.config['from_pickle']
+		self.pickle_data = self.config['pickle_data']
+		self.load()
+
+	def _load_config(self, filename):
+		"""
+		Đọc file config.py và trả về dictionary cho tất cả các tham sô
+		"""
+		return eval(open(filename).read())
+
+	def load(self):
+		"""
+		Load tập cifar vào trong python dictionary
+		"""
+
+		if self.from_pickle:
+			try:
+				with open(os.path.join(self.folder, self.pickle_data), 'rb') as fin:
+					self.cif = cPickle.load(fin)
+					if self.verbose:
+						print 'Loaded {} images each {} pixels'.format(self.cif['images'].shape[0], self.img_size)
+					return self.cif
+			except IOError:
+				print 'You have to provide a (*.pickle) to load data from'
+				sys.exit(0)
+		else:
+			filenames = [os.path.join(self.folder, f) for f in os.listdir(self.folder) if re.match(r'[0-9]+.*\.png', f)]
+			n_images = len(filenames)
+			target = [0]*n_images
+			images = np.zeros((n_images,) + self.img_size)
+
+			for index, filename in enumerate(filenames):
+				image = imread(filename, as_grey=True)
+				image = resize(image, self.img_size)
+				images[index] = image
+
+			if self.verbose:
+				print 'Loaded {} images.'.format(n_images)
+			self.cif = {
+				'images': images,
+				'data': images.reshape((images.shape[0], -1)),
+				'target': np.array(target)
+			}
+
+			now = str(datetime.now()).replace(':', '-')
+			fname_out = 'images-{}-{}-{}.pickle'.format(len(target), self.img_size, now)
+			full_name = os.path.join(self.folder, fname_out)
+			with open(full_name, 'wb') as fout:
+				cPickle.dump(self.cif, fout, -1)
+
+			return self.cif
+
+	def plot_some(self):
+		"""
+		vẽ 100 ảnh kèm nhãn một cách ngẫu nhiên lấy từ tập dữ liệu đã load
+		"""
+		n_images = self.cif['images'].shape[0]
+
+		fig = plt.figure(figsize=(12,12))
+		fig.subplots_adjust(
+			left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+		for i, j in enumerate(np.random.choice(n_images, 100)):
+			ax = fig.add_subplot(10, 10, i + 1, xticks =[], yticks = [])
+			ax.imshow(self.cif['images'][j], cmap = 'Greys_r')
+			ax.text(2, 7, str(self.cif['target'][j]), fontsize = 25, color='red')
+		plt.show() 
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from ImagePreprocessingAndTextDetection import ImageData
+﻿from ImagePreprocessingAndTextDetection import ImageData
+from cifarKaggle import CifarKaggle
 
 if __name__ == '__main__':
+    cifar = CifarKaggle('cifar-config.py')
+    
     test = ImageData("test2.jpg")
     test.plot_preprocessed_image()
     candidates = test.get_text_candidates()
     test.plot_to_check(candidates,'Total Objects Detected')
+    
